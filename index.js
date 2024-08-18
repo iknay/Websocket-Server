db81c1a87e29b83e94996cca2d002b81b35c1ccf
Yancee Villanueva <yanceevillanueva16@gmail.com>
2024-08-18 17:25:57 +0800

    Add headers

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors(corsOptions));
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  // addTrailingSlash: false,
+  addTrailingSlash: false,
   cors: {
     origin: "https://websocket-client-36ea.onrender.com",
     methods: ["GET", "POST"],
